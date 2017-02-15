c20c30ea06014dca58e57738bbaca1ecd8010c3b
Joe Torraca <joe@joetorraca.com>
2017-02-14 19:53:23 -0500

    Initial work on login view

--- app/index.js
+++ app/index.js
@@ -4,30 +4,82 @@ import React, {Component} from 'react';
 
 import {
 	View,
+	Alert,
+	Button,
+	Image,
 	Text,
+	TextInput,
 	StyleSheet
 } from 'react-native';
 
+const onButtonPress = () => {
+  Alert.alert('Button has been pressed!');
+};
+
+export class Login extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			Email: '',
+			Password: ''
+		};
+	}
+	render() {
+		return (
+			<View style={styles.fullscreen}>
+				<View style={{alignItems: 'center'}}>
+					<Image
+						style={{width: 80, height: 175}}
+						source={require('./images/droplet.png')}
+					/>
+				</View>
+				<TextInput
+					style={{height: 40, borderColor: 'gray', borderWidth: 1, padding: 10, marginTop: 100, marginLeft: 20, marginRight: 20}}
+					onChangeText={(Email) => this.setState({Email})}
+					keyboardType="email-address"
+					placeholder='Email'
+				/>
+				<TextInput
+					style={{height: 40, borderColor: 'gray', borderWidth: 1, padding: 10, marginTop: 20, marginLeft: 20, marginRight: 20}}
+					onChangeText={(Password) => this.setState({Password})}
+					placeholder='Password'
+					secureTextEntry={true}
+				/>
+				<Button
+					onPress={onButtonPress}
+					title="Login"
+					color="#841584"
+					accessibilityLabel="Login"
+				/>
+			</View>
+		)
+	}
+}
+
 export default class water extends Component {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.state = {};
-  }
+		this.state = {};
+	}
 
-  render() {
-    return (
-      <View>
-        <Text>Water App</Text>
-      </View>
-    )
-  }
+	render() {
+		return (
+			<View style={styles.fullscreen}>
+				<Login></Login>
+			</View>
+		)
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  }
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center'
+	},
+	fullscreen: {
+		// alignItems: 'center'
+	}
 });
