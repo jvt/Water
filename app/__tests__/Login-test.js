984ae365b9a0d1666a901cdfe62f950d0ba34821
Will Whitlow <wwhitlow@gatech.edu>
2017-04-09 22:38:15 -0400

    Added an initial test file

--- app/__tests__/Login-test.js
+++ app/__tests__/Login-test.js
@@ -1,4 +1,18 @@
 jest.disableAutomock();
 
 const React = require('React');
+const ReactTestRenderer = require('react-test-renderer');
+const Text = require('Text');
+const TextInput = require('TextInput');
+
+describe('UsernameField', () => {
+    it('renders correctly', () => {
+        const instance  = ReactTestRenderer.create(
+            <TextInput style={{}}
+            />
+        );
+
+        expect(instance.toJSON()).toMatchSnapshot();
+    });
+});
 
