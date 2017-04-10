c4d2b979781110fab1c55e0aec3e59a821eb3bb9
Will Whitlow <wwhitlow@gatech.edu>
2017-04-10 14:37:03 -0400

    Added more detail to the Login-test

--- app/__tests__/Login-test.js
+++ app/__tests__/Login-test.js
@@ -4,6 +4,7 @@ const React = require('React');
 const ReactTestRenderer = require('react-test-renderer');
 const Text = require('Text');
 const TextInput = require('TextInput');
+const Button = require('Button');
 
 describe('UsernameField', () => {
     it('renders correctly', () => {
@@ -16,3 +17,40 @@ describe('UsernameField', () => {
     });
 });
 
+describe('PasswordField', () => {
+    it('renders correctly', () => {
+        const instance  = ReactTestRenderer.create(
+            <TextInput style={{}}
+            />
+        );
+
+        expect(instance.toJSON()).toMatchSnapshot();
+    });
+});
+
+describe('LoginButton', () => {
+    it('renders correctly', () => {
+        const instance  = ReactTestRenderer.create(
+            <Button 
+            style={{}}
+            title="Login">
+            Login
+            </Button>
+        );
+
+        expect(instance.toJSON()).toMatchSnapshot();
+    });
+});
+
+describe('CancelButton', () => {
+    it('renders correctly', () => {
+        const instance  = ReactTestRenderer.create(
+            <Button style={{}}
+            title="Return">
+            Return
+            </Button>
+        );
+
+        expect(instance.toJSON()).toMatchSnapshot();
+    });
+});
