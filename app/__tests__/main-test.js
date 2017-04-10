e194e79b137c962af684382f147781c040431a93
ItsDaSpecialK <ItsDaSpecialK@gmail.com>
2017-04-10 18:36:59 -0400

    Added more test cases for main

--- app/__tests__/main-test.js
+++ app/__tests__/main-test.js
@@ -17,3 +17,59 @@ describe('ProfileButton', () => {
         expect(instance.toJSON()).toMatchSnapshot();
     });
 });
+
+describe('NewReportButton', () => {
+    it('renders correctly', () => {
+        const instance  = Renderer.create(
+            <Button 
+            style={{}}
+            title="NewReport">
+            New Report
+            </Button>
+        );
+
+        expect(instance.toJSON()).toMatchSnapshot();
+    });
+});
+
+describe('ShowAllReportsButton', () => {
+    it('renders correctly', () => {
+        const instance  = Renderer.create(
+            <Button 
+            style={{}}
+            title="ShowAllReports">
+            Show All Reports
+            </Button>
+        );
+
+        expect(instance.toJSON()).toMatchSnapshot();
+    });
+});
+
+describe('ViewMapButton', () => {
+    it('renders correctly', () => {
+        const instance  = Renderer.create(
+            <Button 
+            style={{}}
+            title="ViewMap">
+            View Map
+            </Button>
+        );
+
+        expect(instance.toJSON()).toMatchSnapshot();
+    });
+});
+
+describe('LogoutButton', () => {
+    it('renders correctly', () => {
+        const instance  = Renderer.create(
+            <Button 
+            style={{}}
+            title="Logout">
+            Logout
+            </Button>
+        );
+
+        expect(instance.toJSON()).toMatchSnapshot();
+    });
+});
