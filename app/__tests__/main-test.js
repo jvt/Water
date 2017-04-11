324325f27854651e45b6b73d7aa7bf71e136724c
ItsDaSpecialK <ItsDaSpecialK@gmail.com>
2017-04-10 20:06:39 -0400

    Reimplemented the test for main. Fixed an issue where es6 wouldn’t be transpiled in jest, so ‘transformIgnorePaterns’ needed to be set.

--- app/__tests__/main-test.js
+++ app/__tests__/main-test.js
@@ -1,75 +1,13 @@
 jest.disableAutomock();
 
+import 'react-native';
 import React from 'react';
+import Main from '../main'
 import Renderer from 'react-test-renderer';
-import Button from 'Button'
 
-describe('ProfileButton', () => {
-    it('renders correctly', () => {
-        const instance  = Renderer.create(
-            <Button 
-            style={{}}
-            title="Profile">
-            Profile
-            </Button>
-        );
-
-        expect(instance.toJSON()).toMatchSnapshot();
-    });
-});
-
-describe('NewReportButton', () => {
-    it('renders correctly', () => {
-        const instance  = Renderer.create(
-            <Button 
-            style={{}}
-            title="NewReport">
-            New Report
-            </Button>
-        );
-
-        expect(instance.toJSON()).toMatchSnapshot();
-    });
-});
-
-describe('ShowAllReportsButton', () => {
-    it('renders correctly', () => {
-        const instance  = Renderer.create(
-            <Button 
-            style={{}}
-            title="ShowAllReports">
-            Show All Reports
-            </Button>
-        );
-
-        expect(instance.toJSON()).toMatchSnapshot();
-    });
-});
-
-describe('ViewMapButton', () => {
-    it('renders correctly', () => {
-        const instance  = Renderer.create(
-            <Button 
-            style={{}}
-            title="ViewMap">
-            View Map
-            </Button>
-        );
-
-        expect(instance.toJSON()).toMatchSnapshot();
-    });
-});
-
-describe('LogoutButton', () => {
-    it('renders correctly', () => {
-        const instance  = Renderer.create(
-            <Button 
-            style={{}}
-            title="Logout">
-            Logout
-            </Button>
-        );
-
-        expect(instance.toJSON()).toMatchSnapshot();
-    });
+test('renders correctly', () => {
+  const tree = Renderer.create(
+    <Main />
+  ).toJSON();
+  expect(tree).toMatchSnapshot();
 });
