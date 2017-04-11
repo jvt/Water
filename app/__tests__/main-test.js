319c98410e45c03a5dc13406a383cc556c328b0d
ItsDaSpecialK <ItsDaSpecialK@gmail.com>
2017-04-10 20:16:50 -0400

    Did a thing.

--- app/__tests__/main-test.js
+++ app/__tests__/main-test.js
@@ -5,9 +5,11 @@ import React from 'react';
 import Main from '../main'
 import Renderer from 'react-test-renderer';
 
-test('renders correctly', () => {
-  const tree = Renderer.create(
-    <Main />
-  ).toJSON();
-  expect(tree).toMatchSnapshot();
+describe('Main', () => {
+	test('renders correctly', () => {
+		const tree = Renderer.create(
+		<Main />
+		).toJSON();
+		expect(tree).toMatchSnapshot();
+	});
 });
