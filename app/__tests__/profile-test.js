04f27661b33f6d78b9d2813c85b26f0f2a10f712
ItsDaSpecialK <ItsDaSpecialK@gmail.com>
2017-04-23 18:43:57 -0400

    Why was sublime indenting with spaces?? Ugh.

--- app/__tests__/profile-test.js
+++ app/__tests__/profile-test.js
@@ -1,7 +1,7 @@
 import {
-    TextInput,
-    Text,
-    Image
+	TextInput,
+	Text,
+	Image
 } from 'react-native';
 import React from 'react';
 import Profile from '../profile'
@@ -13,133 +13,133 @@ import Renderer from 'react-test-renderer';
 describe('Profile', () => {
 	test('renders correctly', () => {
 		const profile = Renderer.create(
-            <Profile />
-        ).toJSON();
-        expect(profile).toMatchSnapshot();
+			<Profile />
+		).toJSON();
+		expect(profile).toMatchSnapshot();
 	})
 
 	test('MyAccountText', ()  => {
-        const instance = Renderer.create(
-            <Text
-            Style={{}}>
-            My Account
-            </Text>
-        );
+		const instance = Renderer.create(
+			<Text
+			Style={{}}>
+			My Account
+			</Text>
+		);
 
-        expect(instance.toJSON()).toMatchSnapshot();
-    });
+		expect(instance.toJSON()).toMatchSnapshot();
+	});
 
-    test('UsernameText', ()  => {
-        const instance = Renderer.create(
-            <Text
-            Style={{}}>
-            Username:
-            </Text>
-        );
+	test('UsernameText', ()  => {
+		const instance = Renderer.create(
+			<Text
+			Style={{}}>
+			Username:
+			</Text>
+		);
 
-        expect(instance.toJSON()).toMatchSnapshot();
-    });
+		expect(instance.toJSON()).toMatchSnapshot();
+	});
 
-    test('UsernameField', ()  => {
-        const instance = Renderer.create(
-            <TextInput
-            Style={{}}
-            Username
-            />
-        );
+	test('UsernameField', ()  => {
+		const instance = Renderer.create(
+			<TextInput
+			Style={{}}
+			Username
+			/>
+		);
 
-        expect(instance.toJSON()).toMatchSnapshot();
-    });
+		expect(instance.toJSON()).toMatchSnapshot();
+	});
 
-    test('HomeAddressText', ()  => {
-        const instance = Renderer.create(
-            <Text
-            Style={{}}>
-            Home Address:
-            </Text>
-        );
+	test('HomeAddressText', ()  => {
+		const instance = Renderer.create(
+			<Text
+			Style={{}}>
+			Home Address:
+			</Text>
+		);
 
-        expect(instance.toJSON()).toMatchSnapshot();
-    });
+		expect(instance.toJSON()).toMatchSnapshot();
+	});
 
-    test('HomeAddressField', ()  => {
-        const instance = Renderer.create(
-            <TextInput
-            Style={{}}
-            HomeAddress
-            />
-        );
+	test('HomeAddressField', ()  => {
+		const instance = Renderer.create(
+			<TextInput
+			Style={{}}
+			HomeAddress
+			/>
+		);
 
-        expect(instance.toJSON()).toMatchSnapshot();
-    });
+		expect(instance.toJSON()).toMatchSnapshot();
+	});
 
-    test('RoleText', ()  => {
-        const instance = Renderer.create(
-            <Text
-            Style={{}}>
-            Role:
-            </Text>
-        );
+	test('RoleText', ()  => {
+		const instance = Renderer.create(
+			<Text
+			Style={{}}>
+			Role:
+			</Text>
+		);
 
-        expect(instance.toJSON()).toMatchSnapshot();
-    });
+		expect(instance.toJSON()).toMatchSnapshot();
+	});
 
-    test('RoleTabs', ()  => {
-        const instance = Renderer.create(
-            <SegmentedControlTab
-            values={['User', 'Worker', 'Manager', 'Admin']}
+	test('RoleTabs', ()  => {
+		const instance = Renderer.create(
+			<SegmentedControlTab
+			values={['User', 'Worker', 'Manager', 'Admin']}
 			selectedIndex={0}
-            Style={{}}
-            Role
-            />
-        );
+			Style={{}}
+			Role
+			/>
+		);
 
-        expect(instance.toJSON()).toMatchSnapshot();
-    });
+		expect(instance.toJSON()).toMatchSnapshot();
+	});
 
-    test('TitleText', ()  => {
-        const instance = Renderer.create(
-            <Text
-            Style={{}}>
-            Title:
-            </Text>
-        );
+	test('TitleText', ()  => {
+		const instance = Renderer.create(
+			<Text
+			Style={{}}>
+			Title:
+			</Text>
+		);
 
-        expect(instance.toJSON()).toMatchSnapshot();
-    });
+		expect(instance.toJSON()).toMatchSnapshot();
+	});
 
-    test('TitleField', ()  => {
-        const instance = Renderer.create(
-            <TextInput
-            Style={{}}
-            Title
-            />
-        );
+	test('TitleField', ()  => {
+		const instance = Renderer.create(
+			<TextInput
+			Style={{}}
+			Title
+			/>
+		);
 
-        expect(instance.toJSON()).toMatchSnapshot();
-    });
+		expect(instance.toJSON()).toMatchSnapshot();
+	});
 
-    test('SaveButton', () => {
-        const instance = Renderer.create(
-            <Button
-            Style={{}}>
-            Save
-            </Button>
-        );
+	test('SaveButton', () => {
+		const instance = Renderer.create(
+			<Button
+			Style={{}}>
+			Save
+			</Button>
+		);
 
-        expect(instance.toJSON()).toMatchSnapshot();
-    });
+		expect(instance.toJSON()).toMatchSnapshot();
+	});
 
-    test('ReturnButton', () => {
-        const instance = Renderer.create(
-            <Button
-            Style={{}}>
-            Return
-            </Button>
-        );
+	test('ReturnButton', () => {
+		const instance = Renderer.create(
+			<Button
+			Style={{}}>
+			Return
+			</Button>
+		);
 
-        expect(instance.toJSON()).toMatchSnapshot();
-    });
+		expect(instance.toJSON()).toMatchSnapshot();
+	});
 
 
 })
--- app/__tests__/registration-test.js
+++ app/__tests__/registration-test.js
@@ -1,7 +1,7 @@
 import {
-    TextInput,
-    Image,
-    Text
+	TextInput,
+	Image,
+	Text
 } from 'react-native';
 import Button from 'apsl-react-native-button';
 import React from 'react';
@@ -11,108 +11,108 @@ import SegmentedControlTab from 'react-native-segmented-control-tab';
 import BusyIndicator from 'react-native-busy-indicator';
 
 describe('Registration', () => {
-    test('renders correctly', ()  => {
-        const login = Renderer.create(
-            <Registration />
-        ).toJSON();
-        expect(login).toMatchSnapshot();
-    });
+	test('renders correctly', ()  => {
+		const login = Renderer.create(
+			<Registration />
+		).toJSON();
+		expect(login).toMatchSnapshot();
+	});
 
-    test('WaterImage', () => {
-    	const instance = Renderer.create(
-    		<Image
-    		Style={{}}
-    		Droplet
-    		/>
-    	);
+	test('WaterImage', () => {
+		const instance = Renderer.create(
+			<Image
+			Style={{}}
+			Droplet
+			/>
+		);
 
-    	expect(instance.toJSON()).toMatchSnapshot();
-    })
+		expect(instance.toJSON()).toMatchSnapshot();
+	})
 
-    test('RegisterText', ()  => {
-        const instance = Renderer.create(
-            <Text
-            Style={{}}>
-            Register
-            </Text>
-        );
+	test('RegisterText', ()  => {
+		const instance = Renderer.create(
+			<Text
+			Style={{}}>
+			Register
+			</Text>
+		);
 
-        expect(instance.toJSON()).toMatchSnapshot();
-    });
+		expect(instance.toJSON()).toMatchSnapshot();
+	});
 
-    test('UsernameField', ()  => {
-        const instance = Renderer.create(
-            <TextInput
-            Style={{}}
-            Username
-            />
-        );
+	test('UsernameField', ()  => {
+		const instance = Renderer.create(
+			<TextInput
+			Style={{}}
+			Username
+			/>
+		);
 
-        expect(instance.toJSON()).toMatchSnapshot();
-    });
+		expect(instance.toJSON()).toMatchSnapshot();
+	});
 
-    test('PasswordField', () => {
-        const instance = Renderer.create(
-            <TextInput
-            Style={{}}
-            Password
-            />
-        );
+	test('PasswordField', () => {
+		const instance = Renderer.create(
+			<TextInput
+			Style={{}}
+			Password
+			/>
+		);
 
-        expect(instance.toJSON()).toMatchSnapshot();
-    });
+		expect(instance.toJSON()).toMatchSnapshot();
+	});
 
-    test('ConfirmPasswordField', () => {
-        const instance = Renderer.create(
-            <TextInput
-            Style={{}}
-            Confirm Password
-            />
-        );
+	test('ConfirmPasswordField', () => {
+		const instance = Renderer.create(
+			<TextInput
+			Style={{}}
+			Confirm Password
+			/>
+		);
 
-        expect(instance.toJSON()).toMatchSnapshot();
-    });
+		expect(instance.toJSON()).toMatchSnapshot();
+	});
 
-    test('RoleTabs', ()  => {
-        const instance = Renderer.create(
-            <SegmentedControlTab
-            values={['User', 'Worker', 'Manager', 'Admin']}
+	test('RoleTabs', ()  => {
+		const instance = Renderer.create(
+			<SegmentedControlTab
+			values={['User', 'Worker', 'Manager', 'Admin']}
 			selectedIndex={0}
-            Style={{}}
-            Role
-            />
-        );
+			Style={{}}
+			Role
+			/>
+		);
 
-        expect(instance.toJSON()).toMatchSnapshot();
-    });
+		expect(instance.toJSON()).toMatchSnapshot();
+	});
 
-    test('RegisterButton', () => {
-        const instance = Renderer.create(
-            <Button
-            Style={{}}>
-            Register
-            </Button>
-        );
+	test('RegisterButton', () => {
+		const instance = Renderer.create(
+			<Button
+			Style={{}}>
+			Register
+			</Button>
+		);
 
-        expect(instance.toJSON()).toMatchSnapshot();
-    });
+		expect(instance.toJSON()).toMatchSnapshot();
+	});
 
-    test('ReturnButton', () => {
-        const instance = Renderer.create(
-            <Button
-            Style={{}}>
-            Return
-            </Button>
-        );
+	test('ReturnButton', () => {
+		const instance = Renderer.create(
+			<Button
+			Style={{}}>
+			Return
+			</Button>
+		);
 
-        expect(instance.toJSON()).toMatchSnapshot();
-    });
+		expect(instance.toJSON()).toMatchSnapshot();
+	});
 
-    test('BusyIndicator', () =>  {
-        const instance = Renderer.create(
-            <BusyIndicator />
-        );
+	test('BusyIndicator', () =>  {
+		const instance = Renderer.create(
+			<BusyIndicator />
+		);
 
-        expect(instance.toJSON()).toMatchSnapshot();
-    });
+		expect(instance.toJSON()).toMatchSnapshot();
+	});
 });
