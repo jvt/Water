809bc6c5bded6e59ac0b54e3f314d258766f3025
ItsDaSpecialK <ItsDaSpecialK@gmail.com>
2017-04-23 18:21:17 -0400

    Muchos Moar Unit Testses

--- app/__tests__/profile-test.js
+++ app/__tests__/profile-test.js
@@ -17,4 +17,129 @@ describe('Profile', () => {
         ).toJSON();
         expect(profile).toMatchSnapshot();
 	})
+
+	test('MyAccountText', ()  => {
+        const instance = Renderer.create(
+            <Text
+            Style={{}}>
+            My Account
+            </Text>
+        );
+
+        expect(instance.toJSON()).toMatchSnapshot();
+    });
+
+    test('UsernameText', ()  => {
+        const instance = Renderer.create(
+            <Text
+            Style={{}}>
+            Username:
+            </Text>
+        );
+
+        expect(instance.toJSON()).toMatchSnapshot();
+    });
+
+    test('UsernameField', ()  => {
+        const instance = Renderer.create(
+            <TextInput
+            Style={{}}
+            Username
+            />
+        );
+
+        expect(instance.toJSON()).toMatchSnapshot();
+    });
+
+    test('HomeAddressText', ()  => {
+        const instance = Renderer.create(
+            <Text
+            Style={{}}>
+            Home Address:
+            </Text>
+        );
+
+        expect(instance.toJSON()).toMatchSnapshot();
+    });
+
+    test('HomeAddressField', ()  => {
+        const instance = Renderer.create(
+            <TextInput
+            Style={{}}
+            HomeAddress
+            />
+        );
+
+        expect(instance.toJSON()).toMatchSnapshot();
+    });
+
+    test('RoleText', ()  => {
+        const instance = Renderer.create(
+            <Text
+            Style={{}}>
+            Role:
+            </Text>
+        );
+
+        expect(instance.toJSON()).toMatchSnapshot();
+    });
+
+    test('RoleTabs', ()  => {
+        const instance = Renderer.create(
+            <SegmentedControlTab
+            values={['User', 'Worker', 'Manager', 'Admin']}
+			selectedIndex={0}
+            Style={{}}
+            Role
+            />
+        );
+
+        expect(instance.toJSON()).toMatchSnapshot();
+    });
+
+    test('TitleText', ()  => {
+        const instance = Renderer.create(
+            <Text
+            Style={{}}>
+            Title:
+            </Text>
+        );
+
+        expect(instance.toJSON()).toMatchSnapshot();
+    });
+
+    test('TitleField', ()  => {
+        const instance = Renderer.create(
+            <TextInput
+            Style={{}}
+            Title
+            />
+        );
+
+        expect(instance.toJSON()).toMatchSnapshot();
+    });
+
+    test('SaveButton', () => {
+        const instance = Renderer.create(
+            <Button
+            Style={{}}>
+            Save
+            </Button>
+        );
+
+        expect(instance.toJSON()).toMatchSnapshot();
+    });
+
+    test('ReturnButton', () => {
+        const instance = Renderer.create(
+            <Button
+            Style={{}}>
+            Return
+            </Button>
+        );
+
+        expect(instance.toJSON()).toMatchSnapshot();
+    });
+
+
 })
