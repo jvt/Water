c2110d0fb4cfe76995cbd77a246850c23cdd5462
Will Whitlow <wwhitlow@gatech.edu>
2017-04-26 07:40:13 -0400

    Added Conditional Rendering to admin

--- app/main.js
+++ app/main.js
@@ -7,6 +7,7 @@ import {
 	Navigator,
 	Text,
 	TextInput,
+  ScrollView,
 	StyleSheet
 } from 'react-native';
 
@@ -14,7 +15,30 @@ import styles from './styles';
 import Button from 'apsl-react-native-button';
 
 export class Main extends Component {
-	render() {
+	constructor(props) {
+    super(props);
+
+    this.state = {
+      user: {},
+      admin: false
+    };
+
+    AsyncStorage.getItem('@water2340:user', function(e, user) {
+      let _parsed = JSON.parse(user);
+      console.log(_parsed);
+      updateUser(_parsed);
+    });
+
+    const updateUser = user => {
+      this.setState({user: user});
+      if(this.state.user.role == 3){
+        this.setState({admin: true});
+      }
+      console.log(this.state)
+    }
+  }
+
+  render() {
 		const _navigateToWelcome = () => {
 			AsyncStorage.removeItem('@water2340:user');
 			this.props.shouldSkipToApp = false;
@@ -65,42 +89,48 @@ export class Main extends Component {
 					style={{fontSize: 20, textAlign: 'center', fontWeight: '200'}}>
 					Clean Water
 				</Text>
-				<Button
-					style={{backgroundColor: 'rgba(66, 163, 221, 1)', marginLeft: 20, marginRight: 20, borderWidth: 0, marginTop: 20}}
-					onPress={() => _navigateToProfile()}
-					textStyle={{fontSize: 18}}>
-					Profile
-				</Button>
-				<Button
-					style={{backgroundColor: 'rgba(66, 163, 221, 1)', marginLeft: 20, marginRight: 20, borderWidth: 0, marginTop: 20}}
-					onPress={() => _navigateToNewReport()}
-					textStyle={{fontSize: 18}}>
-					New Report
-				</Button>
-				<Button
-					style={{backgroundColor: 'rgba(66, 190, 110, 1)', marginLeft: 20, marginRight: 20, borderWidth: 0, marginTop: 20}}
-					onPress={() => _navigateToAllReports()}
-					textStyle={{fontSize: 18}}>
-					Show All Reports
-				</Button>
-				<Button
-					style={{backgroundColor: 'rgba(66, 190, 110, 1)', marginLeft: 20, marginRight: 20, borderWidth: 0, marginTop: 20}}
-					onPress={() => _navigateToMap()}
-					textStyle={{fontSize: 18}}>
-					View Map
-				</Button>
-        <Button
-          style={{backgroundColor: 'rgba(232, 88, 74, 1)', marginLeft: 20, marginRight: 20, borderWidth: 0, marginTop: 20}}
-          onPress={() => _navigateToAdmin()}
-          textStyle={{fontSize: 18}}>
-          Admin Panel
-        </Button>
-				<Button
-					style={{backgroundColor: 'rgba(232, 88, 74, 1)', marginLeft: 20, marginRight: 20, borderWidth: 0, marginTop: 20}}
-					onPress={() => _navigateToWelcome()}
-					textStyle={{fontSize: 18}}>
-					Logout
-				</Button>
+        <ScrollView>
+  				<Button
+  					style={{backgroundColor: 'rgba(66, 163, 221, 1)', marginLeft: 20, marginRight: 20, borderWidth: 0, marginTop: 20}}
+  					onPress={() => _navigateToProfile()}
+  					textStyle={{fontSize: 18}}>
+  					Profile
+  				</Button>
+  				<Button
+  					style={{backgroundColor: 'rgba(66, 163, 221, 1)', marginLeft: 20, marginRight: 20, borderWidth: 0, marginTop: 20}}
+  					onPress={() => _navigateToNewReport()}
+  					textStyle={{fontSize: 18}}>
+  					New Report
+  				</Button>
+  				<Button
+  					style={{backgroundColor: 'rgba(66, 190, 110, 1)', marginLeft: 20, marginRight: 20, borderWidth: 0, marginTop: 20}}
+  					onPress={() => _navigateToAllReports()}
+  					textStyle={{fontSize: 18}}>
+  					Show All Reports
+  				</Button>
+  				<Button
+  					style={{backgroundColor: 'rgba(66, 190, 110, 1)', marginLeft: 20, marginRight: 20, borderWidth: 0, marginTop: 20}}
+  					onPress={() => _navigateToMap()}
+  					textStyle={{fontSize: 18}}>
+  					View Map
+  				</Button>
+          <View>
+            {this.state.admin && 
+            <Button
+              style={{backgroundColor: 'rgba(232, 88, 74, 1)', marginLeft: 20, marginRight: 20, borderWidth: 0, marginTop: 20}}
+              onPress={() => _navigateToAdmin()}
+              textStyle={{fontSize: 18}}>
+              Admin Panel
+            </Button> 
+          }
+          </View>
+  				<Button
+  					style={{backgroundColor: 'rgba(232, 88, 74, 1)', marginLeft: 20, marginRight: 20, borderWidth: 0, marginTop: 20}}
+  					onPress={() => _navigateToWelcome()}
+  					textStyle={{fontSize: 18}}>
+  					Logout
+  				</Button>
+        </ScrollView>
 			</View>
 		)
 	}
